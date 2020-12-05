14b51cddffce9683d1073d64c4a35399258ff00d
muddassar <muddassarsaleem89@gmail.com>
2020-12-05 22:45:47 +0500

    Testing

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from Ui import main_screen
 
 
 def init():
-	databaseobject = Database_class.Database()
-	connection = databaseobject.create_connection(databasename="database.db")
-	databaseobject.create_tables()
-	studentcontroller = Student_controller.Student_controller(connection=connection)
-	return [studentcontroller]
+    databaseobject = Database_class.Database()
+    connection = databaseobject.create_connection(databasename="database.db")
+    databaseobject.create_tables()
+    studentcontroller = Student_controller.Student_controller(connection=connection)
+    return [studentcontroller]
 
 
+# Main Function
 def main():
-	controller_objects = init()
-	main_screen.mainmenu(controller_objects)
+    controller_objects = init()
+    main_screen.mainmenu(controller_objects)
 
 
 main()
